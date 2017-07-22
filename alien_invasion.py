0990125c2745590b970a3d51f8a8f1c5b4ac4a71
LR <q209209a@163.com>
2017-07-22 19:26:54 +0800

    Format Code，with github destop client

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,16 +8,17 @@ import game_functions as gf
 def run_game():
     pygame.init()
 
-    ai_setting=Settings()
-    screen=pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
+    ai_setting = Settings()
+    screen = pygame.display.set_mode(
+        (ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
-    ship=Ship(screen)
+    ship = Ship(screen)
 
     while True:
         gf.check_events(ship)
         ship.update()
-        gf.update_screen(ai_setting,screen,ship)
+        gf.update_screen(ai_setting, screen, ship)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings():
     '''存储游戏里所有设置的类'''
 
     def __init__(self):
-        self.screen_width=1200
-        self.screen_height=800
-        self.bg_color=(230,230,230)
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
